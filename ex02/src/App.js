de8e2ec8056f2dcb907c63009174d01b27e39715
Mirela <adisserdarevic@gmail.com>
2021-03-31 22:42:21 +0200

    Added folder ex02

---
+++ ex02/src/App.js
@@ -0,0 +1,27 @@
+//getcookie,localstorage,sessionstorage/App.js
+import React, { Component } from 'react';
+class App extends Component {
+  //setMyStorage
+  setMyStorage = () => {
+    document.cookie = 'Year=2021';
+    localStorage.setItem('Paragon', 'yes, but Arena first');
+    sessionStorage.setItem('frontend', 'React');
+  };
+  //getMyStorage
+  getMyStorage = () => {
+  let myCookieData = document.cookie.getItem('Year');
+  let myLocalStorageData = localStorage.getItem('Paragon');
+  let mySessionStorageData = sessionStorage.getItem('frontend');
+  console.log(this.getMyStorage(myCookieData, myLocalStorageData, mySessionStorageData));
+  };
+  //return 
+  render() {
+    return (
+      <div className='App'>
+        <button type='button' onClick={this.setMyStorage}>Set my storage</button>
+        <button type='button' onClick={this.getMyStorage}>Get my storage</button>
+      </div>
+    );
+  }
+}
+export default App; //exported App;
